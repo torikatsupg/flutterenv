17f6bef6e4564b6588fbcb83d8f4a39a4f3ab49c
torikatsu <torikatsu923@gmail.com>
2022-01-10 16:54:42 +0900

    modify dart-defines parser

--- ios/scripts/dart_define.sh
+++ ios/scripts/dart_define.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-echo "#include \"$(echo $DART_DEFINES | base64 -d | tr ',' '\n' | grep 'FLAVOR' | sed 's/.*=//').xcconfig\"" > $SRCROOT/Flutter/DartDefine.xcconfig
+#/bin/bash
+echo $DART_DEFINE | tr ',' '\n' | while read line; 
+do
+  echo $line | base64 -d | tr ',' '\n' | xargs -I@ bash -c "echo @ | grep 'FLAVOR' | sed 's/.*=//'"
+done | (
+  read flavor
+  echo "#include \"$flavor.xcconfig\"" > $SRCROOT/Flutter/DartDefine.xcconfig
+)
